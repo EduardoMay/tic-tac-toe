4b001773535e7c91156027ae6b3381f5bf54077c
EduardoMay <eduardo_may@outlook.com>
2020-09-19 11:42:23 -0500

    resolviendo de forma vertical

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const winners = [
     ['0', '4', '8'],
     ['2', '4', '6'],
 ];
+const tablePlayer = [['', '', ''], ['', '', ''], ['', '', '']];
 
 let pointsPlayer1 = 0, pointsPlayer2 = 0,  player1 = true, player2 = false;
 
@@ -20,21 +21,93 @@ gameItems.forEach(item => {
 });
 
 function changePlayer(e) {
+    const dataGame = Number(e.target.dataset['game']);
+
     if(player1) {
-        console.log('player 1');
         e.target.dataset['player'] = 1;
         e.target.classList += ' selected';
         e.target.textContent = 'X';
 
+        setItemPlayer(dataGame, 1);
+        resolverHorizontalPlayer('X');
+
         player1 = false;
         player2 = true;
     } else if(player2) {
-        console.log('player 2');
         e.target.dataset['player'] = 2;
         e.target.classList += ' selected';
         e.target.textContent = 'O';
 
+        setItemPlayer(dataGame, 2);
+        resolverHorizontalPlayer('O');
+
         player1 = true;
         player2 = false;
+    }
+}
+
+function setItemPlayer(dataGame, dataPlayer) {
+    if (dataGame >= 0 && dataGame < 3) {
+        if (dataPlayer === 1) {
+            tablePlayer[0][dataGame] = 'X';
+        }
+
+        if(dataPlayer === 2) {
+            tablePlayer[0][dataGame] = 'O';
+        }
+    } else if (dataGame >= 3 && dataGame < 6) {
+        if (dataPlayer === 1) {
+            tablePlayer[1][dataGame - 3] = 'X';
+        }
+
+        if(dataPlayer === 2) {
+            tablePlayer[1][dataGame - 3] = 'O';
+        }
+    } else {
+        if (dataPlayer === 1) {
+            tablePlayer[2][dataGame - 6] = 'X';
+        }
+
+        if(dataPlayer === 2) {
+            tablePlayer[2][dataGame - 6] = 'O';
+        }
+    }
+}
+
+function resolverHorizontalPlayer(itemPlayer) {
+    console.table(tablePlayer)
+    for (let i = 0; i < 3; i++) {
+        const items = tablePlayer[i];
+
+        if(items.every(item => item === itemPlayer)) {
+            switch (i) {
+                case 0:
+                    for (let i = 0; i < 3; i++) {
+                        document.querySelector(`[data-game='${i}']`).classList.add('winner');
+                    }
+                    break;
+                case 1:
+                    for (let i = 3; i < 6; i++) {
+                        document.querySelector(`[data-game='${i}']`).classList.add('winner');
+                    }
+                    break;
+
+                default:
+                    for (let i = 6; i < 9; i++) {
+                        document.querySelector(`[data-game='${i}']`).classList.add('winner');
+                    }
+                    break;
+            }
+            console.log('Ganastes');
+
+            if(player1) {
+                console.log('Player 1');
+                document.querySelector('#winner').innerHTML = 'Gano X';
+            } else {
+                console.log('Player 2');
+                document.querySelector('#winner').innerHTML = 'Gano O';
+            }
+            break;
+        }
     }
 }
