4f48e56dee731c89db08309d8ce7518b73cde512
EduardoMay <eduardo_may@outlook.com>
2020-09-26 12:21:22 -0500

    prettier

--- main.js
+++ main.js
@@ -2,280 +2,315 @@ document.querySelector('#points-1').innerHTML = localStorage.player1 || 0;
 document.querySelector('#points-2').innerHTML = localStorage.player2 || 0;
 
 const options = {
-    automatico: true,
-    manual: false
-}
+	automatico: true,
+	manual: false,
+};
 const winners = [
-    ['0', '1', '2'],
-    ['3', '4', '5'],
-    ['6', '7', '8'],
-    ['0', '3', '6'],
-    ['1', '4', '7'],
-    ['2', '5', '8'],
-    ['0', '4', '8'],
-    ['2', '4', '6'],
+	['0', '1', '2'],
+	['3', '4', '5'],
+	['6', '7', '8'],
+	['0', '3', '6'],
+	['1', '4', '7'],
+	['2', '5', '8'],
+	['0', '4', '8'],
+	['2', '4', '6'],
 ];
-let tablePlayer = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+let tablePlayer = [
+	['-', '-', '-'],
+	['-', '-', '-'],
+	['-', '-', '-'],
+];
 
-let pointsPlayer1 = 0, pointsPlayer2 = 0,  player1 = true, player2 = false;
+let pointsPlayer1 = 0,
+	pointsPlayer2 = 0,
+	player1 = true,
+	player2 = false;
 
-const gameItems = document.querySelectorAll(".game-item");
+const gameItems = document.querySelectorAll('.game-item');
 
-gameItems.forEach(item => {
-    item.addEventListener("click", e => {
-        changePlayer(e);
-    });
+gameItems.forEach((item) => {
+	item.addEventListener('click', (e) => {
+		changePlayer(e);
+	});
 });
 
 function changePlayer(e) {
-    const dataGame = Number(e.target.dataset['game']);
+	const dataGame = Number(e.target.dataset['game']);
 
-    if(player1) {
-        e.target.dataset['player'] = 1;
-        e.target.classList += ' selected';
-        e.target.textContent = 'X';
+	if (player1) {
+		e.target.dataset['player'] = 1;
+		e.target.classList += ' selected';
+		e.target.textContent = 'X';
 
-        setItemPlayer(dataGame, 1);
-        resolverHorizontalPlayer('X');
-        resolverVerticalPlayer('X');
-        resolverDiagonalPlayer('X');
+		setItemPlayer(dataGame, 1);
+		resolverHorizontalPlayer('X');
+		resolverVerticalPlayer('X');
+		resolverDiagonalPlayer('X');
 
-        if (options.automatico) {
-            player1 = false;
-            player2 = true;
-        }
-    } else if(player2) {
-        e.target.dataset['player'] = 2;
-        e.target.classList += ' selected';
-        e.target.textContent = 'O';
+		if (options.automatico) {
+			player1 = false;
+			player2 = true;
+		}
+	} else if (player2) {
+		e.target.dataset['player'] = 2;
+		e.target.classList += ' selected';
+		e.target.textContent = 'O';
 
-        setItemPlayer(dataGame, 2);
-        resolverHorizontalPlayer('O');
-        resolverVerticalPlayer('O');
-        resolverDiagonalPlayer('O');
+		setItemPlayer(dataGame, 2);
+		resolverHorizontalPlayer('O');
+		resolverVerticalPlayer('O');
+		resolverDiagonalPlayer('O');
 
-        if (options.automatico) {
-            player1 = true;
-            player2 = false;
-        }
-    }
+		if (options.automatico) {
+			player1 = true;
+			player2 = false;
+		}
+	}
 }
 
 function setItemPlayer(dataGame, dataPlayer) {
-    if (dataGame >= 0 && dataGame < 3) {
-        if (dataPlayer === 1) {
-            tablePlayer[0][dataGame] = 'X';
-        }
+	if (dataGame >= 0 && dataGame < 3) {
+		if (dataPlayer === 1) {
+			tablePlayer[0][dataGame] = 'X';
+		}
 
-        if(dataPlayer === 2) {
-            tablePlayer[0][dataGame] = 'O';
-        }
-    } else if (dataGame >= 3 && dataGame < 6) {
-        if (dataPlayer === 1) {
-            tablePlayer[1][dataGame - 3] = 'X';
-        }
+		if (dataPlayer === 2) {
+			tablePlayer[0][dataGame] = 'O';
+		}
+	} else if (dataGame >= 3 && dataGame < 6) {
+		if (dataPlayer === 1) {
+			tablePlayer[1][dataGame - 3] = 'X';
+		}
 
-        if(dataPlayer === 2) {
-            tablePlayer[1][dataGame - 3] = 'O';
-        }
-    } else {
-        if (dataPlayer === 1) {
-            tablePlayer[2][dataGame - 6] = 'X';
-        }
+		if (dataPlayer === 2) {
+			tablePlayer[1][dataGame - 3] = 'O';
+		}
+	} else {
+		if (dataPlayer === 1) {
+			tablePlayer[2][dataGame - 6] = 'X';
+		}
 
-        if(dataPlayer === 2) {
-            tablePlayer[2][dataGame - 6] = 'O';
-        }
-    }
+		if (dataPlayer === 2) {
+			tablePlayer[2][dataGame - 6] = 'O';
+		}
+	}
 }
 
 function resolverHorizontalPlayer(itemPlayer) {
-    for (let i = 0; i < 3; i++) {
-        const items = tablePlayer[i];
+	for (let i = 0; i < 3; i++) {
+		const items = tablePlayer[i];
 
-        if(items.every(item => item === itemPlayer)) {
-            switch (i) {
-                case 0:
-                    document.querySelector(`[data-game='1']`).classList.add('winner-h');
-                    break;
-                case 1:
-                    document.querySelector(`[data-game='4']`).classList.add('winner-h');
-                    break;
+		if (items.every((item) => item === itemPlayer)) {
+			switch (i) {
+				case 0:
+					document
+						.querySelector(`[data-game='1']`)
+						.classList.add('winner-h');
+					break;
+				case 1:
+					document
+						.querySelector(`[data-game='4']`)
+						.classList.add('winner-h');
+					break;
 
-                default:
-                    document.querySelector(`[data-game='7']`).classList.add('winner-h');
-                    break;
-            }
+				default:
+					document
+						.querySelector(`[data-game='7']`)
+						.classList.add('winner-h');
+					break;
+			}
 
-            printTextWinner(player1);
+			printTextWinner(player1);
 
-            break;
-        }
-    }
+			break;
+		}
+	}
 }
 
 function resolverVerticalPlayer(itemPlayer) {
-    let tableVertical = [
-        [ tablePlayer[0][0], tablePlayer[1][0], tablePlayer[2][0] ],
-        [ tablePlayer[0][1], tablePlayer[1][1], tablePlayer[2][1] ],
-        [ tablePlayer[0][2], tablePlayer[1][2], tablePlayer[2][2] ],
-    ];
+	let tableVertical = [
+		[tablePlayer[0][0], tablePlayer[1][0], tablePlayer[2][0]],
+		[tablePlayer[0][1], tablePlayer[1][1], tablePlayer[2][1]],
+		[tablePlayer[0][2], tablePlayer[1][2], tablePlayer[2][2]],
+	];
 
-    for (let i = 0; i < 3; i++) {
-        const items = tableVertical[i];
+	for (let i = 0; i < 3; i++) {
+		const items = tableVertical[i];
 
-        if(items.every(item => item === itemPlayer)) {
-            switch (i) {
-                case 0:
-                        document.querySelector(`[data-game='0']`).classList.add('winner-v');
-                    break;
-                case 1:
-                        document.querySelector(`[data-game='1']`).classList.add('winner-v');
-                    break;
+		if (items.every((item) => item === itemPlayer)) {
+			switch (i) {
+				case 0:
+					document
+						.querySelector(`[data-game='0']`)
+						.classList.add('winner-v');
+					break;
+				case 1:
+					document
+						.querySelector(`[data-game='1']`)
+						.classList.add('winner-v');
+					break;
 
-                default:
-                        document.querySelector(`[data-game='2']`).classList.add('winner-v');
-                    break;
-            }
+				default:
+					document
+						.querySelector(`[data-game='2']`)
+						.classList.add('winner-v');
+					break;
+			}
 
-            printTextWinner(player1);
+			printTextWinner(player1);
 
-            break;
-        }
-
-    }
+			break;
+		}
+	}
 }
 
 function resolverDiagonalPlayer(itemPlayer) {
-    let tableVertical = [
-        [ tablePlayer[0][0], tablePlayer[1][1], tablePlayer[2][2] ],
-        [ tablePlayer[0][2], tablePlayer[1][1], tablePlayer[2][0] ],
-    ];
+	let tableVertical = [
+		[tablePlayer[0][0], tablePlayer[1][1], tablePlayer[2][2]],
+		[tablePlayer[0][2], tablePlayer[1][1], tablePlayer[2][0]],
+	];
 
-    for (let i = 0; i < 2; i++) {
-        const items = tableVertical[i];
+	for (let i = 0; i < 2; i++) {
+		const items = tableVertical[i];
 
-        if(items.every(item => item === itemPlayer)) {
-            switch (i) {
-                case 0:
-                        document.querySelector(`[data-game='0']`).classList.add('winner-d-1');
-                    break;
+		if (items.every((item) => item === itemPlayer)) {
+			switch (i) {
+				case 0:
+					document
+						.querySelector(`[data-game='0']`)
+						.classList.add('winner-d-1');
+					break;
 
-                default:
-                        document.querySelector(`[data-game='2']`).classList.add('winner-d-2');
-                    break;
-            }
+				default:
+					document
+						.querySelector(`[data-game='2']`)
+						.classList.add('winner-d-2');
+					break;
+			}
 
-            printTextWinner(player1);
+			printTextWinner(player1);
 
-            break;
-        }
-    }
+			break;
+		}
+	}
 }
 
 function printTextWinner(player) {
-    (player)
-        ? document.querySelector('#winner').innerHTML = 'Ganador Equipo 1'
-        : document.querySelector('#winner').innerHTML = 'Ganador Equipo 2';
+	player
+		? (document.querySelector('#winner').innerHTML = 'Ganador Equipo 1')
+		: (document.querySelector('#winner').innerHTML = 'Ganador Equipo 2');
 
-    const pointsNow1 = Number(localStorage.player1);
-    const pointsNow2 = Number(localStorage.player2);
+	const pointsNow1 = Number(localStorage.player1);
+	const pointsNow2 = Number(localStorage.player2);
 
-    if (player) {
-        if (pointsNow1) {
-            localStorage.setItem('player1', pointsNow1 + 1);
-        } else {
-            localStorage.setItem('player1', 1);
-        }
+	if (player) {
+		if (pointsNow1) {
+			localStorage.setItem('player1', pointsNow1 + 1);
+		} else {
+			localStorage.setItem('player1', 1);
+		}
 
-        document.querySelector('#points-1').innerHTML = localStorage.player1;
-    } else {
-        if (pointsNow2) {
-            localStorage.setItem('player2', pointsNow2 + 1);
-        } else {
-            localStorage.setItem('player2', 1);
-        }
+		document.querySelector('#points-1').innerHTML = localStorage.player1;
+	} else {
+		if (pointsNow2) {
+			localStorage.setItem('player2', pointsNow2 + 1);
+		} else {
+			localStorage.setItem('player2', 1);
+		}
 
-        document.querySelector('#points-2').innerHTML = localStorage.player2;
-    }
-
+		document.querySelector('#points-2').innerHTML = localStorage.player2;
+	}
 }
 
-document.querySelector("#clear").addEventListener('click', () => {
-    tablePlayer = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+document.querySelector('#clear').addEventListener('click', () => {
+	tablePlayer = [
+		['-', '-', '-'],
+		['-', '-', '-'],
+		['-', '-', '-'],
+	];
 
-    document.querySelector('#winner').innerHTML = '';
+	document.querySelector('#winner').innerHTML = '';
 
-    gameItems.forEach((item, index) => {
-        item.classList.remove('winner-h');
-        item.classList.remove('winner-v');
-        item.classList.remove('winner-d-1');
-        item.classList.remove('winner-d-2');
-        item.classList.remove('selected');
-        item.dataset['player'] = '-';
-        item.innerHTML = index + 1;
-    });
+	gameItems.forEach((item, index) => {
+		item.classList.remove('winner-h');
+		item.classList.remove('winner-v');
+		item.classList.remove('winner-d-1');
+		item.classList.remove('winner-d-2');
+		item.classList.remove('selected');
+		item.dataset['player'] = '-';
+		item.innerHTML = index + 1;
+	});
 });
 
-document.querySelector("#reset").addEventListener('click', () => {
-    tablePlayer = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+document.querySelector('#reset').addEventListener('click', () => {
+	tablePlayer = [
+		['-', '-', '-'],
+		['-', '-', '-'],
+		['-', '-', '-'],
+	];
 
-    document.querySelector('#winner').innerHTML = '';
+	document.querySelector('#winner').innerHTML = '';
 
-    localStorage.removeItem('player1');
-    localStorage.removeItem('player2');
+	localStorage.removeItem('player1');
+	localStorage.removeItem('player2');
 
-    document.querySelector('#points-1').innerHTML = 0;
-    document.querySelector('#points-2').innerHTML = 0;
+	document.querySelector('#points-1').innerHTML = 0;
+	document.querySelector('#points-2').innerHTML = 0;
 
-    gameItems.forEach((item, index) => {
-        item.classList.remove('winner-h');
-        item.classList.remove('winner-v');
-        item.classList.remove('winner-d-1');
-        item.classList.remove('winner-d-2');
-        item.classList.remove('selected');
-        item.dataset['player'] = '-';
-        item.innerHTML = index + 1;
-    });
+	gameItems.forEach((item, index) => {
+		item.classList.remove('winner-h');
+		item.classList.remove('winner-v');
+		item.classList.remove('winner-d-1');
+		item.classList.remove('winner-d-2');
+		item.classList.remove('selected');
+		item.dataset['player'] = '-';
+		item.innerHTML = index + 1;
+	});
 });
 
-(player1)
-    ? document.querySelector('#player-1').classList.add("selected")
-    : document.querySelector('#player-2').classList.add("selected");
+const btnPlayer1 = document
+	.querySelector('#player-1')
+	.classList.add('selected');
+const btnPlayer2 = document
+	.querySelector('#player-2')
+	.classList.add('selected');
+const elementChangePlay = document.querySelector('#change-options');
+const btnGameType = document.querySelector('#game-type');
 
-document.querySelector('#player-1').addEventListener('click', _ => {
-    player1 = !player1;
-    player2 = false;
+player1 ? btnPlayer1 : btnPlayer2;
 
-    document.querySelector('#player-1').classList.add("selected");
-    document.querySelector('#player-2').classList.remove("selected");
+btnPlayer1.addEventListener('click', (_) => {
+	player1 = !player1;
+	player2 = false;
+
+	btnPlayer1.classList.add('selected');
+	btnPlayer2.classList.remove('selected');
 });
 
-document.querySelector('#player-2').addEventListener('click', _ => {
-    player1 = false;
-    player2 = !player2;
+btnPlayer2.addEventListener('click', (_) => {
+	player1 = false;
+	player2 = !player2;
 
-    document.querySelector('#player-1').classList.remove("selected");
-    document.querySelector('#player-2').classList.add("selected");
+	btnPlayer1.classList.remove('selected');
+	btnPlayer2.classList.add('selected');
 });
 
 if (options.automatico) {
-    document.querySelector("#change-options").classList.add('d-none');
+	elementChangePlay.classList.add('d-none');
 }
 
-document.querySelector("#game-type").addEventListener('click', e => {
-    const type = Number(e.target.dataset['gameType']);
+gameType.addEventListener('click', (e) => {
+	const type = Number(e.target.dataset['gameType']);
 
-    if (type === 1) {
-        options.automatico = false;
-        options.manual = true;
-        e.target.dataset['gameType'] = 2;
-        document.querySelector("#change-options").classList.remove('d-none');
-    } else {
-        options.automatico = true;
-        options.manual = false;
-        e.target.dataset['gameType'] = 1;
-        document.querySelector("#change-options").classList.add('d-none');
-    }
-
-})
+	if (type === 1) {
+		options.automatico = false;
+		options.manual = true;
+		e.target.dataset['gameType'] = 2;
+		elementChangePlay.classList.remove('d-none');
+	} else {
+		options.automatico = true;
+		options.manual = false;
+		e.target.dataset['gameType'] = 1;
+		elementChangePlay.classList.add('d-none');
+	}
+});
